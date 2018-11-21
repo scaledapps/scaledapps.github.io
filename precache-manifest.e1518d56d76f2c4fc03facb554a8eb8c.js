3c51a4a88dd7c68149536113f305ec85904292b9
Gurunath Sripad <guruanth.s@brilliio.com>
2018-11-21 10:28:08 +0530

    Initial version of app

---
+++ precache-manifest.e1518d56d76f2c4fc03facb554a8eb8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6abe280da9dc99a51ee",
+    "url": "/static/js/main.f6abe280.chunk.js"
+  },
+  {
+    "revision": "7431cf0387f9fc5e9023",
+    "url": "/static/js/1.7431cf03.chunk.js"
+  },
+  {
+    "revision": "f6abe280da9dc99a51ee",
+    "url": "/static/css/main.aed23bea.chunk.css"
+  },
+  {
+    "revision": "c502913edbe34bbf31b064d72491d0ae",
+    "url": "/index.html"
+  }
+];
